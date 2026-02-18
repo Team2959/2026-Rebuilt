a10b4ce7e8948dbb904c06800bef9c9da99bd2d5
Kevin Russell <kevin_russell@leco.com>
2026-02-17 21:40:16 -0500

    clean up

--- src/main/java/frc/robot/vision/AprilTagHelpers.java
+++ src/main/java/frc/robot/vision/AprilTagHelpers.java
@@ -13,38 +13,38 @@ public class AprilTagHelpers {
     private final static double hubY = 4.0;
 
     public static double distanceToHub() {
+        var pose3d = alliancePose();
+
         // calculate distance from robot to hub
         // =SQRT(B14*B14+B15*B15)
-        var deltaX = deltaXToHub();
-        var deltaY = deltaYToHub();
+        var deltaX = deltaXToHub(pose3d);
+        var deltaY = deltaYToHub(pose3d);
 
         return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
     }
 
     public static double turretAngleToHub(double currentRobotRotation) {
+        var pose3d = alliancePose();
+
         // B14 = DeltaX
         // B15 = DeltaY
         // =IF(B14 = 0, 0, ATAN(B15/B14))
-        var deltaX = deltaXToHub();
+        var deltaX = deltaXToHub(pose3d);
         if (deltaX == 0)
             return 0;
 
-        var fieldAngle = Math.atan(deltaYToHub() / deltaX);
+        var fieldAngle = Math.atan(deltaYToHub(pose3d) / deltaX);
 
         return Math.toDegrees(fieldAngle) - currentRobotRotation;
     }
 
     // need to get x and y forom limelight
-    private static double deltaXToHub() {
-        var pose3d = alliancePose();
-        var robotX = pose3d.getX();
-        return hubX - robotX;
+    private static double deltaXToHub(Pose3d pose3d) {
+        return hubX - pose3d.getX();
     }
 
-    private static double deltaYToHub() {
-        var pose3d = alliancePose();
-        var robotY = pose3d.getY();
-        return hubY - robotY;
+    private static double deltaYToHub(Pose3d pose3d) {
+        return hubY - pose3d.getY();
     }
 
     // what do you do if the limelight doesn't see an april tag????
